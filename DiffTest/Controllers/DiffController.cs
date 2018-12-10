4b90d4c1ea0234ad6236ac22922901c740073b2c
BljahVljah <bljahvljah@gmail.com>
2018-12-10 17:01:50 +0100

    GET done

--- DiffTest/Controllers/DiffController.cs
+++ DiffTest/Controllers/DiffController.cs
@@ -10,13 +10,27 @@ namespace DiffTest.Controllers
     public class DiffController : ApiController
     {
 
-        // GET: api/Diff/5
-        public string Get(int id)
+        // GET example v1/diff/1
+        [Route("v1/diff/{id}")]
+        public object Get(string id)
         {
-            return "value";
+            //TODO Tadej: I can delete this. CHECK IT before!!! changed Lists to Dictionaries
+            //DataInput inputToFindLeft = dataInputsLeft.Find(x => x.id == id); 
+            //DataInput inputToFindRight = dataInputsRight.Find(x => x.id == id);
+            DataInput inputToFindLeft = new DataInput();
+            DataInput inputToFindRight = new DataInput();
+
+            if (!dataInputsLeft.TryGetValue(id, out inputToFindLeft) || !dataInputsRight.TryGetValue(id, out inputToFindRight))
+                if (inputToFindLeft == null || inputToFindRight == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            return CompareInputs(inputToFindLeft.data, inputToFindRight.data);
         }
 
-        //PUT example v1/diff//1/left
+        //PUT example v1/diff/1/left
+        //body {data:"AAAAAA=="}
         [Route("v1/diff/{id}/{sideLR}")]
         public HttpResponseMessage Put(string id, string sideLR, [FromBody]DataInput input)
         {
@@ -68,6 +82,80 @@ namespace DiffTest.Controllers
                     currList.Add(id, input);
                 }
 
+            }
+        }
+
+
+        public class DiffResult
+        {
+            public int offset;
+            public int length;
+
+        }
+
+        public class ReturnResult
+        {
+            public string diffResultType;
+            public List<DiffResult> diffs;
+
+            public bool ShouldSerializediffs()
+            {
+                return (diffs != null);
+            }
+
+        }
+
+
+        public static ReturnResult CompareInputs(string inputLeft, string inputRight)
+        {
+
+            ReturnResult returnRes = new ReturnResult();
+            if (inputLeft.Equals(inputRight))
+            {
+                returnRes.diffResultType = "Equals";
+                return returnRes;
+            }
+            else if (inputLeft.Length != inputRight.Length)
+            {
+                returnRes.diffResultType = "SizeDoNotMatch";
+                return returnRes;
+            }
+            else
+            {
+                List<DiffResult> diffResults = new List<DiffResult>();
+
+                char[] charLeft = inputLeft.ToCharArray();
+                char[] charRight = inputRight.ToCharArray();
+
+                int offset = 0;
+                int length = 0;
+
+                for (int i = 0; i < charLeft.Length; i++)
+                {
+                    if (charLeft[i] == charRight[i])
+                    {
+                        if (length != 0)
+                        {
+
+                            DiffResult diff = new DiffResult
+                            {
+                                offset = offset,
+                                length = length
+                            };
+                            diffResults.Add(diff);
+                        }
+                        offset = i + 1;
+                        length = 0;
+                    }
+                    else
+                    {
+                        length++;
+                    }
+                }
+                
+                returnRes.diffResultType = "ContentDoNotMatch";
+                returnRes.diffs = diffResults;
+                return returnRes;
             }
         }
 
